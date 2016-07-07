14cb64ff21ac650fc496073e95573222cd66b91c
enami tsugutomo <t.enami@sonyged.com>
2016-07-07 11:37:56 +0900

    Add missing file.

---
+++ compile.js
@@ -0,0 +1,22 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*-
+ *
+ */
+
+'use strict';
+const debug = require('debug')('compile');
+const biltrans = require('./biltrans.js');
+
+const scripts = (() => {
+  if (process.argv.length > 2) {
+    const fs = require('fs');
+    const scripts = fs.readFileSync(process.argv[2]);
+    return JSON.parse(scripts);
+  } else {
+    const scripts = require('./example/example2.js').scripts;
+    return scripts;
+  }
+})();
+
+const trans = biltrans.translator(scripts);
+console.log(trans.translate());
+
