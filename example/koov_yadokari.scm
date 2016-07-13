875d487a56084cb74a65d421f9ae2d1850b09434
enami tsugutomo <t.enami@sonyged.com>
2016-07-13 12:29:44 +0900

    Add generator for yadokari and isoginchaku programs.

---
+++ example/koov_yadokari.scm
@@ -0,0 +1,21 @@
+;;;
+;;; cd ..
+;;; gosh ./example/koov_yadokari.scm  | jq .
+
+(add-load-path "./example")
+(use block)
+
+;; yadokari 0713
+(define *scripts*
+  '((when-green-flag-clicked
+     (forever
+      (servomotor-synchronized-motion
+       15
+       (set-servomotor-degree D9 30))
+      (servomotor-synchronized-motion
+       15
+       (set-servomotor-degree D9 150))))))
+(define *port-mappings*
+  '((D9 . V5)))
+
+(block-list->json *scripts* *port-mappings*)
