ffd4ed881e87c230523c761c6facbe78fb8b168d
enami tsugutomo <t.enami@sonyged.com>
2016-07-13 13:46:59 +0900

    Make this simple.

--- example/koov_asahigani.scm
+++ example/koov_asahigani.scm
@@ -5,18 +5,19 @@
 (add-load-path "./example")
 (use block)
 
+(define (servomotor-synchronized-motion speed port&degree)
+  `(servomotor-synchronized-motion
+    ,speed
+    ,@(map (^[p&d] `(set-servomotor-degree ,(car p&d) ,(cdr p&d)))
+           port&degree)))
+
 ;;; koov_asahigani_0706.bpd
 (define *scripts*
-  '((when-green-flag-clicked
+  `((when-green-flag-clicked
      (forever
-      (servomotor-synchronized-motion
-       10
-       (set-servomotor-degree D2 90)
-       (set-servomotor-degree D4 90)
-       (set-servomotor-degree D9 90)
-       (set-servomotor-degree D10 90)
-       (set-servomotor-degree D11 90)
-       (set-servomotor-degree D12 90))
+      ,(servomotor-synchronized-motion
+        10
+        '((D2 . 90) (D4 . 90) (D9 . 90) (D10 . 90) (D11 . 90) (D12 . 90)))
       (if-then
        (= (touch-sensor-value A0) 0)
        (wait 0.5)
@@ -33,63 +34,32 @@
      eat
      (repeat-until
       (= (touch-sensor-value A1) 1)
-      (servomotor-synchronized-motion
-       8
-       (set-servomotor-degree D2 125)
-       (set-servomotor-degree D4 90))
-      (servomotor-synchronized-motion
-       10
-       (set-servomotor-degree D2 90)
-       (set-servomotor-degree D4 90))
-      (servomotor-synchronized-motion
-       8
-       (set-servomotor-degree D2 90)
-       (set-servomotor-degree D4 125))
-      (servomotor-synchronized-motion
-       10
-       (set-servomotor-degree D2 90)
-       (set-servomotor-degree D4 90))))
+      ,(servomotor-synchronized-motion 8 '((D2 . 125) (D4 . 90)))
+      ,(servomotor-synchronized-motion 10 '((D2 . 90) (D4 . 90)))
+      ,(servomotor-synchronized-motion 8 '((D2 . 90) (D4 . 125)))
+      ,(servomotor-synchronized-motion 10 '((D2 . 90) (D4 . 90)))))
     (function
      walk01
      (repeat-until
       (= (touch-sensor-value A0) 1)
-      (servomotor-synchronized-motion
-       14
-       (set-servomotor-degree D2 95)
-       (set-servomotor-degree D4 105)
-       (set-servomotor-degree D9 75)
-       (set-servomotor-degree D10 65)
-       (set-servomotor-degree D11 105)
-       (set-servomotor-degree D12 115))
-      (servomotor-synchronized-motion
-       14
-       (set-servomotor-degree D2 105)
-       (set-servomotor-degree D4 95)
-       (set-servomotor-degree D9 115)
-       (set-servomotor-degree D10 105)
-       (set-servomotor-degree D11 65)
-       (set-servomotor-degree D12 75))))
+      ,(servomotor-synchronized-motion
+        14
+        '((D2 . 95) (D4 . 105) (D9 . 75) (D10 . 65) (D11 . 105) (D12 . 115)))
+      ,(servomotor-synchronized-motion
+        14
+        '((D2 . 105) (D4 . 95) (D9 . 115) (D10 . 105) (D11 . 65) (D12 . 75)))))
     (function
      walk02
      (repeat-until
       (and (= (touch-sensor-value A0) 1) (= (touch-sensor-value A1) 1))
-      (servomotor-synchronized-motion
-       14
-       (set-servomotor-degree D2 120)
-       (set-servomotor-degree D4 120)
-       (set-servomotor-degree D9 90)
-       (set-servomotor-degree D10 75)
-       (set-servomotor-degree D11 105)
-       (set-servomotor-degree D12 105))
-      (servomotor-synchronized-motion
-       14
-       (set-servomotor-degree D2 90)
-       (set-servomotor-degree D4 90)
-       (set-servomotor-degree D9 105)
-       (set-servomotor-degree D10 90)
-       (set-servomotor-degree D11 75)
-       (set-servomotor-degree D12 75))))
+      ,(servomotor-synchronized-motion
+        14
+        '((D2 . 120) (D4 . 120) (D9 . 90) (D10 . 75) (D11 . 105) (D12 . 105)))
+      ,(servomotor-synchronized-motion
+        14
+        '((D2 . 90) (D4 . 90) (D9 . 105) (D10 . 90) (D11 . 75) (D12 . 75)))))
     ))
+
 (define *port-mappings*
   '((A0 . K6) (A1 . K7)
     (D2 . V2) (D4 . V3)
