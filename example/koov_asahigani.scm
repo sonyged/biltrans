98eabac59ebc80a2ae2547f080c1ae539e49f0e0
enami tsugutomo <t.enami@sonyged.com>
2016-08-04 08:10:05 +0900

    Issue set-servomotor-degree on startup.

--- example/koov_asahigani.scm
+++ example/koov_asahigani.scm
@@ -8,10 +8,12 @@
 ;;; koov_asahigani_0706.bpd
 (define *scripts*
   `((when-green-flag-clicked
+     ,@(map (^[args] `(set-servomotor-degree ,@args))
+            '((D2 90) (D4 90) (D9 90) (D10 90) (D11 90) (D12 90)))
      (forever
       ,(servomotor-synchronized-motion
         10
-        '((D2 . 90) (D4 . 90) (D9 . 90) (D10 . 90) (D11 . 90) (D12 . 90)))
+        '((D2 90) (D4 90) (D9 90) (D10 90) (D11 90) (D12 90)))
       (if-then
        (= (touch-sensor-value A0) 0)
        (wait 0.5)
@@ -28,30 +30,30 @@
      eat
      (repeat-until
       (= (touch-sensor-value A1) 1)
-      ,(servomotor-synchronized-motion 8 '((D2 . 125) (D4 . 90)))
-      ,(servomotor-synchronized-motion 10 '((D2 . 90) (D4 . 90)))
-      ,(servomotor-synchronized-motion 8 '((D2 . 90) (D4 . 125)))
-      ,(servomotor-synchronized-motion 10 '((D2 . 90) (D4 . 90)))))
+      ,(servomotor-synchronized-motion 8 '((D2 125) (D4 90)))
+      ,(servomotor-synchronized-motion 10 '((D2 90) (D4 90)))
+      ,(servomotor-synchronized-motion 8 '((D2 90) (D4 125)))
+      ,(servomotor-synchronized-motion 10 '((D2 90) (D4 90)))))
     (function
      walk01
      (repeat-until
       (= (touch-sensor-value A0) 1)
       ,(servomotor-synchronized-motion
         14
-        '((D2 . 95) (D4 . 105) (D9 . 75) (D10 . 65) (D11 . 105) (D12 . 115)))
+        '((D2 95) (D4 105) (D9 75) (D10 65) (D11 105) (D12 115)))
       ,(servomotor-synchronized-motion
         14
-        '((D2 . 105) (D4 . 95) (D9 . 115) (D10 . 105) (D11 . 65) (D12 . 75)))))
+        '((D2 105) (D4 95) (D9 115) (D10 105) (D11 65) (D12 75)))))
     (function
      walk02
      (repeat-until
       (and (= (touch-sensor-value A0) 1) (= (touch-sensor-value A1) 1))
       ,(servomotor-synchronized-motion
         14
-        '((D2 . 120) (D4 . 120) (D9 . 90) (D10 . 75) (D11 . 105) (D12 . 105)))
+        '((D2 120) (D4 120) (D9 90) (D10 75) (D11 105) (D12 105)))
       ,(servomotor-synchronized-motion
         14
-        '((D2 . 90) (D4 . 90) (D9 . 105) (D10 . 90) (D11 . 75) (D12 . 75)))))
+        '((D2 90) (D4 90) (D9 105) (D10 90) (D11 75) (D12 75)))))
     ))
 
 (define *port-mappings*
