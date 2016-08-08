027f45e9550084483948d8310f81f4fe4155c153
enami tsugutomo <t.enami@sonyged.com>
2016-08-08 18:38:05 +0900

    Add special version for video.

---
+++ example/crocodile_bite_0809.scm
@@ -0,0 +1,142 @@
+;;; -*- indent-tabs-mode: nil -*-
+;;; cd ..
+;;; gosh ./example/crocodile_bite.scm  | jq .
+
+(add-load-path "./example")
+(use block)
+(use gauche.parseopt)
+
+;(define *sensor-op* '(< light-sensor-value 50))
+;(let-args *argv*
+;  ((#f "ir" => (^[] (set! *sensor-op* '(> ir-photo-reflector-value 10))))))
+
+(define *sensor-op* '(> ir-photo-reflector-value 30))
+
+(define (sensor-reacted port)
+  `(,(car *sensor-op*) (,(cadr *sensor-op*) ,port) ,(caddr *sensor-op*)))
+
+;; crocodile_bite
+(define *scripts*
+  `((when-green-flag-clicked
+     (if-then
+      (= (button-value A0) 0)
+      (call-function "photo"))
+     (if-then
+      (= (button-value A1) 0)
+      (call-function "kamituki")))
+     
+    (function
+     photo
+     (forever
+      (set-servomotor-degree D9 120)
+      (turn-led A4 OFF)
+      (turn-led A5 OFF)
+      (wait 2)
+
+      (if-then
+       ,(sensor-reacted 'A6)
+       (set-variable-to bite (pick-random 1 3))
+       (if-then
+        (= (variable-ref bite) 1)
+        (repeat
+         5
+         (turn-led A4 ON)
+         (turn-led A5 OFF)
+         (wait 0.2)
+         (turn-led A4 OFF)
+         (turn-led A5 ON)
+         (wait 0.2))
+        (turn-led A5 ON)
+        (call-function OK)
+        (set-servomotor-degree D9 150)
+        (wait 1)
+        (set-servomotor-degree D9 120))
+
+       (if-then
+        (= (variable-ref bite) 2)
+        (repeat
+         5
+         (turn-led A4 ON)
+         (turn-led A5 OFF)
+         (wait 0.2)
+         (turn-led A4 OFF)
+         (turn-led A5 ON)
+         (wait 0.2))
+        (call-function OK)
+        (repeat
+         3
+         (set-servomotor-degree D9 140)
+         (wait 0.2)
+         (set-servomotor-degree D9 150)
+         (wait 0.2)))
+  
+       (if-then
+        (= (variable-ref bite) 3)
+        (repeat
+         5
+         (turn-led A4 ON)
+         (turn-led A5 OFF)
+         (wait 0.2)
+         (turn-led A4 OFF)
+         (turn-led A5 ON)
+         (wait 0.2))
+        (turn-led A4 ON)
+        (turn-led A5 OFF)
+        (call-function NG)
+        (repeat
+         3
+         (set-servomotor-degree D9 140)
+         (wait 0.5)
+         (set-servomotor-degree D9 180)
+         (wait 0.5))))))
+
+    (function
+     kamituki
+     (forever
+      (set-servomotor-degree D9 120)
+      (turn-led A4 OFF)
+      (turn-led A5 OFF)
+      (wait 2)
+
+      (if-then
+       (= (touch-sensor-value A3) 0)
+       (repeat
+	5
+	(turn-led A4 ON)
+	(turn-led A5 OFF)
+	(wait 0.2)
+	(turn-led A4 OFF)
+	(turn-led A5 ON)
+	(wait 0.2))
+       (turn-led A4 ON)
+       (turn-led A5 OFF)
+       (call-function NG)
+       (repeat
+	3
+	(set-servomotor-degree D9 140)
+	(wait 0.5)
+	(set-servomotor-degree D9 180)
+	(wait 0.5)))))
+
+    (function
+     OK
+     (buzzer-on A2 72)
+     (wait 0.2)
+     (buzzer-off A2)
+     (wait 0.2)
+     (buzzer-on A2 69)
+     (wait 0.2)
+     (buzzer-off A2))
+
+    (function
+     NG
+     (buzzer-on A2 50)
+     (wait 0.5)
+     (buzzer-off A2))
+
+    (variable bite 0)))
+
+(define *port-mappings*
+  '((A4 . V2) (A5 . V3) (A2 . V6) (A3 . K6) (A6 . K7) (D9 . V9)))
+
+(block-list->json *scripts* *port-mappings*)
---
+++ example/koov_s_armcar_touchsensor_0806.scm
@@ -0,0 +1,72 @@
+;;; -*- indent-tabs-mode: nil -*-
+;;; cd ..
+;;; gosh ./example/koov_s_armcar_touchsensor_0806.scm  | jq .
+
+(add-load-path "./example")
+(use block)
+
+;; s_armcar_touchsensor 0806
+(define *scripts*
+  `((when-green-flag-clicked
+     (set-servomotor-degree D11 160)
+     (set-servomotor-degree D9 20)
+     (if-then
+      (= (button-value A1) 0)
+      (call-function "photo"))
+     (if-then
+      (= (button-value A3) 0)
+      (call-function "no_photo")))
+    (function
+     photo
+     (set-servomotor-degree D11 160)
+     (set-servomotor-degree D9 20)
+     (forever
+      (if-then
+       (> (ir-photo-reflector-value A2) 35)
+       (wait 1)
+       ,(servomotor-synchronized-motion
+	 5
+	 '((D9 160) (D11 20)))
+       (wait 1)
+       (forever
+	(set-dcmotor-power M1 70)
+	(set-dcmotor-power M2 30)
+	(turn-dcmotor-on M1 REVERSE)
+	(turn-dcmotor-on M2 REVERSE)
+	(if-then
+	 (= (touch-sensor-value A0) 0)
+	 (call-function touch))))))
+    (function
+     no_photo
+     (wait 1)
+     ,(servomotor-synchronized-motion
+       5
+       '((D9 160) (D11 20)))
+     (forever
+      (set-dcmotor-power M1 70)
+      (set-dcmotor-power M2 40)
+      (turn-dcmotor-on M1 REVERSE)
+      (turn-dcmotor-on M2 REVERSE)
+      (if-then
+       (= (touch-sensor-value A0) 0)
+       (call-function touch))))
+    (function
+     touch
+     (turn-dcmotor-off M1 BRAKE)
+     (turn-dcmotor-off M2 BRAKE)
+     (wait 1)
+     ,(servomotor-synchronized-motion
+       5
+       '((D11 160) (D9 20)))
+     (wait-until (< (ir-photo-reflector-value A2) 10))
+     (wait 1)
+     (wait-until (> (ir-photo-reflector-value A2) 20))
+     (wait 1)
+     ,(servomotor-synchronized-motion
+       5
+       '((D9 160) (D11 20))))))
+
+(define *port-mappings*
+  '((M1 V0) (M2 V1) (D9 V2) (D11 V3) (A0 K2) (A2 K4) (A1 A1) (A3 A3)))
+
+(block-list->json *scripts* *port-mappings*)
