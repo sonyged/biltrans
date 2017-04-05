390af13293e1ebd51ff86c709816c21fc840d169
enami tsugutomo <t.enami@sonyged.com>
2017-04-05 09:35:41 +0900

    Enable interpreter in factory default sketch.

--- scripts/compile_factory_default.sh
+++ scripts/compile_factory_default.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 export ENABLE_FUNCTION_TEST_01=yes
+export ENABLE_INTERP=yes
 cd $(dirname $0)
 
-node ../compile.js ../example/empty.json | sh ./compile.sh
+BUILDDIR=${1:-/tmp/build}
+SKETCH_CPP=${2:-factory-default.cpp}
+
+cp ../code/interp_insns.h ${BUILDDIR}/
+cat ../code/interp_main.c |
+    sh ./compile.sh ${BUILDDIR} ${SKETCH_CPP}
